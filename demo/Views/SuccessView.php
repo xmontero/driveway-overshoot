f15d001f86c9554438eb9e4f9b562fa7067c2d48
Xavi Montero <xmontero@dsitelecom.com>
2016-06-24 11:52:39 +0200

    Refactors success view to use the widgets.

--- demo/Views/SuccessView.php
+++ demo/Views/SuccessView.php
@@ -6,7 +6,7 @@ class SuccessView
 {
     private $widgets;
 
-    public function __construct( $widgets )
+    public function __construct( AnsiWidgets $widgets )
     {
         $this->widgets = $widgets;
     }
@@ -14,28 +14,23 @@ class SuccessView
     public function render( $data )
     {
         $page = $this->widgets->header();
-        $page .= "Files
------
-* The demo.php is the FRONT-CONTROLLER that sets the autoloader up, and then calls the real controller.
-* The Controllers/DefaultController.php is the CONTROLLER in the MVC pattern.
-* The Views/SuccessView.php is the VIEW in the MVC pattern.
-* The the project itself (under src/) is the MODEL in the MVC pattern.
 
-How it works
-------------
+        $filesContent = "        * The demo.php is the FRONT-CONTROLLER that sets the autoloader up, and then calls the real controller.
+        * The Controllers/DefaultController.php is the CONTROLLER in the MVC pattern.
+        * The Views/SuccessView.php is the VIEW in the MVC pattern.
+        * The the project itself (under src/) is the MODEL in the MVC pattern." . PHP_EOL;
+        $page .= $this->widgets->block( 'FILES', $filesContent );
 
-The CONTROLLER
-a) gets access to the MODEL,
-b) operates it (ie: controls it),
-c) gets its resulting data,
-d) transforms the data into a DTO suitable for the VIEW, and
-e) renders the view.
+        $howItWorksContent = "        The CONTROLLER
+            a) gets access to the MODEL,
+            b) operates it (ie: controls it),
+            c) gets its resulting data,
+            d) transforms the data into a DTO suitable for the VIEW, and
+            e) renders the view." . PHP_EOL;
+        $page .= $this->widgets->block( 'HOW IT WORKS', $howItWorksContent );
 
-Execution
----------
-
-Hello, $data->name!
-";
+        $executionContent = "        Hello, $data->name!" . PHP_EOL;
+        $page .= $this->widgets->block( 'EXECUTION', $executionContent );
 
         return $page;
     }
