775baff993b5bcfe55c46625cc3aab04976b7411
Xavi Montero <xmontero@dsitelecom.com>
2016-07-09 21:10:57 +0200

    Starts to document one-liners for the classes.

--- src/Coordinates.php
+++ src/Coordinates.php
@@ -4,6 +4,12 @@ declare( strict_types = 1 );
 
 namespace XaviMontero\DrivewayOvershoot;
 
+/**
+ * Represents the position of a Tile inside a Sudoku.
+ *
+ * This is a value object that represents the coordinates inside a 9x9 Sudoku.
+ * It is identified by the column and the row ids.
+ */
 class Coordinates
 {
     private $column;
@@ -15,16 +21,29 @@ class Coordinates
         $this->row = $row;
     }
 
+    /**
+     * Returns the id of the column where coordinates belong to.
+     * @return \XaviMontero\DrivewayOvershoot\OneToNineValue
+     */
     public function getColumn() : OneToNineValue
     {
         return $this->column;
     }
 
+    /**
+     * Returns the id of the row where coordinates belong to.
+     * @return \XaviMontero\DrivewayOvershoot\OneToNineValue
+     */
     public function getRow() : OneToNineValue
     {
         return $this->row;
     }
 
+    /**
+     * Returns the id of the 3x3 square where the coordinates belong to.
+     * Squares are numbered 123 in the first three rows, 456 in the three middle rows and 789 in the last three rows.
+     * @return \XaviMontero\DrivewayOvershoot\OneToNineValue
+     */
     public function getSquare() : OneToNineValue
     {
         $x = $this->getColumn()->getValue();
